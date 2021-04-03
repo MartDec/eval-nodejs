d0a1ceb02f17094e3680ea29459cef594f9532f1
Martin Déclert <mdeclert.dev@gmail.com>
2021-04-03 17:14:59 +0200

    Affichage de tous les commentaires d'un article

---
+++ www/js/comments-preview.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const previewOpeners = document.querySelectorAll('.comment-preview-opener')
+
+    previewOpeners.forEach(opener => {
+        const post = opener.getAttribute('post-id')
+        const targetPopup = document.querySelector(`.comments-preview[post-id="${post}"]`)
+        const targetPopupCloser = targetPopup.querySelector('.close-popup')
+        opener.addEventListener('click', event => {
+            event.preventDefault()
+
+            targetPopup.classList.remove('hide')
+            targetPopupCloser.addEventListener('click', event => {
+                event.preventDefault()
+                targetPopup.classList.add('hide')
+            })
+        })
+    })
+
+})
